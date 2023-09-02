6de6ba7e0d086a8dbf7e5dc9077dbaeca3df2d36
AlphaMoonbaseBerlin <kontakt@alphamoonbase.de>
2023-09-03 00:24:45 +0200

    additional exceptions with errors!

--- modules/suspects/project/remote_dependency/github_remote/githubRemote.py
+++ modules/suspects/project/remote_dependency/github_remote/githubRemote.py
@@ -28,26 +28,41 @@ class githubRemote:
 	def getRepoData(self):
 		return [ str(value) for value in re.search(r"github\.com\/([\w,-]+)\/([\w,-]+).*", self.ownerComp.par.Repository.eval()).groups() ]
 	
+	@property
+	def fileRegex(self):
+		return self.ownerComp.par.Fileregex.eval()
+	
 	def searchFile(self, releaseDict:dict):
-		for assetEement in releaseDict["assets"]:
-			if re.match( self.ownerComp.par.Fileregex.eval(), assetEement["name"]): 
-				return assetEement.get("browser_download_url", "" )
+		for assetElement in releaseDict["assets"]:
+			if re.match( self.fileRegex, assetElement["name"]): 
+				return assetElement["browser_download_url"]
+		raise Exception(f"Could not find file with regex {self.fileRegex}")
 
+	def getAndRaise(self, url):
+		response = requests.get( url )
+		response.raise_for_status()
+		return response
+	
 	def fetchLatest(self):
 		owner, repoName = self.getRepoData()[0:2]
 		apiEndpoint = f" https://api.github.com/repos/{owner}/{repoName}/releases/latest"
-		response = requests.get( apiEndpoint )
+		response = self.getAndRaise( apiEndpoint )
 		return self.searchFile( self.checkResponse( response ) )
-
+	
+	@property
+	def tagRegex(self):
+		return self.ownerComp.par.Tagregex.eval()
+	
 	def fetchByTag(self):
 		owner, repoName = self.getRepoData()[0:2]
 		apiEndpoint = f" https://api.github.com/repos/{owner}/{repoName}/releases?per_page={self.ownerComp.par.Searchdepth.eval()}"
-		response = requests.get( apiEndpoint )
+		response = self.getAndRaise( apiEndpoint )
 		for releaseDict in self.checkResponse( response ):
-			if re.match( self.ownerComp.par.Tagregex.eval(), releaseDict["name"]): 
+			if re.match( self.tagRegex , releaseDict["name"]): 
 				return self.searchFile( releaseDict )
-		
+		raise Exception(f"Could not find tag with regex {self.tagRegex}")
 
 	def ExternalData(self):
 		if self.ownerComp.par.Mode.eval() == "Latest": return self.fetchLatest()
 		if self.ownerComp.par.Mode.eval() == "Search Tag": return self.fetchByTag()
+		raise Exception( "Invalid Mode selected", self.ownerComp.par.Mode.eval() )
