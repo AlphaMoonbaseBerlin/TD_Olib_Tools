ff77352d2e063baec80e14a72033f3333fca8874
AlphaMoonbaseBerlin <kontakt@alphamoonbase.de>
2023-11-09 12:20:44 +0100

    made basicly all function of remoteDependency private to not pollute stubs. Added some comments.

--- modules/suspects/project/remote_dependency/extOlibDependancy.py
+++ modules/suspects/project/remote_dependency/extOlibDependancy.py
@@ -2,10 +2,9 @@
 Name : extOlibDependancy
 Author : Wieland@AMB-ZEPH15
 Saveorigin : Project.toe
-Saveversion : 2022.28040
+Saveversion : 2022.32660
 Info Header End'''
 
-from os import path
 import pathlib
 import subprocess
 import td
@@ -15,15 +14,15 @@ class extOlibDependancy:
 
 	def __init__(self, ownerComp):
 		# The component to which this extension is attached
-		self.ownerComp = ownerComp
+		self.ownerComp:COMP = ownerComp
 
-	def filepath(self)-> pathlib.Path:
+	def _filepath(self)-> pathlib.Path:
 		return pathlib.Path( 
 			self.ownerComp.par.Downloaddirectory.eval(),
 			self.ownerComp.par.Filename.eval() 
 		)
 
-	def fetchRemoteData(self):
+	def _fetchRemoteData(self):
 		targetmode = self.ownerComp.par.Target.eval()
 		if targetmode == "Olib"		: remote = self.ownerComp.op("olib_remote")
 		if targetmode == "Github"	: remote = self.ownerComp.op("github_remote")
@@ -31,33 +30,37 @@ class extOlibDependancy:
 		if targetmode == "Callback" : remote = self.ownerComp.op("callback_remote")
 		return remote.ExternalData()
 
-	def GetRemoteFilepath(self):
-		filepath = self.filepath()
+	def GetRemoteFilepath(self) -> pathlib.Path:
+		"""Downloads the file from the selected source and returns the filepath."""
+		filepath = self._filepath()
 		if filepath.is_file(): return filepath
-		downloadURL = self.fetchRemoteData()
-		return self.downloadFile( 	filepath,
+		downloadURL = self._fetchRemoteData()
+		return self._downloadFile( 	filepath,
 									downloadURL )
 	
-	def GetGlobalComponent(self):
+	def GetGlobalComponent(self) -> COMP:
+		"""Downlaods the remote resource and places a Operator representing it with the given
+		Global Shortcut in the define path.
+		If the file does alreay exist and is placed, it just returns the operator."""
 		globalOPShortcut = self.ownerComp.par.Targetopshortcut.eval()
-		return getattr( op, globalOPShortcut, None ) or self.downloadAndPlace()
+		return getattr( op, globalOPShortcut, None ) or self._downloadAndPlace()
 
-	def downloadAndPlace(self):
+	def _downloadAndPlace(self) -> COMP:
 		targetTox = self.GetRemoteFilepath()
 		if not targetTox: return None
 
-		newComp 					= self.getTargetAndPlace().loadTox( targetTox )
+		newComp 					= self._getTargetAndPlace().loadTox( targetTox )
 		newComp.par.opshortcut.val = self.ownerComp.par.Targetopshortcut.eval()
 		return newComp
 
-	def getTargetAndPlace(self):
+	def _getTargetAndPlace(self) -> COMP:
 		operator = td.root
 		for path_element in self.ownerComp.par.Targetplace.val.split("/"):
 			if not path_element: continue
 			operator = operator.op(path_element) or operator.create(baseCOMP, tdu.legalName(path_element))
 		return operator
 
-	def downloadFile(self, filepath:pathlib.Path, url:str):
+	def _downloadFile(self, filepath:pathlib.Path, url:str) -> pathlib.Path:
 		downloadScriptDAT 	= self.ownerComp.op("downloadScript")
 		downloadScript 		= pathlib.Path( f"TDImportCache/Scripts/{downloadScriptDAT.id}" )
 		downloadScript.is_file() or downloadScriptDAT.save(downloadScript, createFolders = True)
